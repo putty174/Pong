5e5f3a35d9db7a4d14ae0035b73e93ccdbd4e6a9
putty174 <myh10232223@yahoo.com>
2014-02-24 18:02:15 -0800

    (Maybe) Start of building client list

--- PongServer/PongServer/Server.cs
+++ PongServer/PongServer/Server.cs
@@ -11,21 +11,28 @@ namespace PongServer
 		{
 			bool done = false;
 			int port = 4000;
-			int attempt = 0;
+			int connected = 0;
+			Socket[] players = new Socket[2];
 
 			IPEndPoint ip = new IPEndPoint (IPAddress.Any, port);
 			UdpClient listener = new UdpClient (port);
+			Socket sock = listener.Client;
 
 			try
 			{
-				while(!done)
+				while(connected < 2)
 				{
-					attempt++;
 					Console.WriteLine ("Waiting for connetion... ");
 					byte[] data = listener.Receive (ref ip);
-					Console.WriteLine("Attempt: " + attempt);
 
 					Console.WriteLine("Recieved broadcast from {0} :\n {1}\n", ip.ToString(), Encoding.ASCII.GetString(data,0,data.Length));
+					Console.WriteLine("Adding {0} to players", ip.ToString());
+					players[connected] = listener.Client;
+					connected++;
+				}
+
+				while(!done)
+				{
 				}
 			}
 			catch (Exception e)
