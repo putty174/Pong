29053dd2448067f724af07c7f0332fa0dd159f1d
putty174 <myh10232223@yahoo.com>
2014-03-03 13:50:44 -0800

    Restructure didn't work well

--- MasterServer/MasterServer/Main.cs
+++ MasterServer/MasterServer/Main.cs
@@ -12,55 +12,48 @@ namespace MasterServer
 {
 	class MainClass
 	{
-		const int maxPlayers = 2;
-		const int portNumber = 4000;
-		int connectedPlayers = 0;
-		TcpListener listener = new TcpListener(IPAddress.Any, portNumber);
-		TcpClient[] clientList = new TcpClient[maxPlayers];
-		Thread listenThread1;
-		Thread listenThread2;
 
 		public static void Main (string[] args)
 		{
 			Console.WriteLine(" >> " + "Welcome to the Pong2D server!");
 			Console.WriteLine(" >> " + "Wainting for clients.....");
 
-			listenThread1 = new Thread (new ThreadStart (ListendForTCPClients));
-			listenThread2 = new Thread (new ThreadStart (ListendForTCPClients));
-
-			listenThread1.Start ();
-			listenThread2.Start ();
-
-			Console.WriteLine("<< 2 clinets have connected to the the Pong2D server");
-			Console.WriteLine("<< Waiting for clients to send the start command....");
+			MainServer ms = new MainServer();
 			
-			byte[] data1 = new byte[1024];
-			byte[] data2 = new byte[1024];
-			
-			String mes1, mes2;
-			while(true)
-			{
-				NetworkStream stream1 = clientList[0].GetStream();
-				NetworkStream stream2 = clientList[1].GetStream();
-				
-				for(int i = stream1.Read(data1,0,data1.Length);i != 0; i = stream1.Read(data1,0,data1.Length))
-				{
-					mes1 = System.Text.Encoding.ASCII.GetString(data1,0,i);
-					Console.WriteLine("Recieved: {0}", mes1);
-				}
-				for(int i = stream2.Read(data2,0,data2.Length);i != 0; i = stream2.Read (data2,0,data2.Length))
-				{
-					mes2 = System.Text.Encoding.ASCII.GetString(data2,0,i);
-					Console.WriteLine("Recieved: {0}", mes2);
-				}
-				Console.WriteLine("");
-			}
 		}
 
+
+
+
+
+	}
+
+	class MainServer : MainClass
+	{
+		const int maxPlayers = 2;
+		const int portNumber = 4000;
+		int connectedPlayers;
+		TcpListener listener;
+		TcpClient[] clientList;
+		Thread listenThread1;
+		Thread listenThread2;
+		
+		public MainServer()
+		{
+			connectedPlayers = 0;
+			clientList = new TcpClient[maxPlayers];
+			listener = new TcpListener(IPAddress.Any, portNumber);
+			listenThread1 = new Thread (new ThreadStart (ListendForTCPClients));
+			listenThread1.Start ();
+			listenThread2 = new Thread (new ThreadStart (ListendForTCPClients));
+			listenThread2.Start ();
+			
+		}
+		
 		public void ListendForTCPClients()
 		{
-			listener.Start();
-			
+			this.listener.Start();
+
 			try
 			{
 				while(connectedPlayers <= maxPlayers - 1)
@@ -69,6 +62,30 @@ namespace MasterServer
 					Console.WriteLine(" >> " + "Client No: " + Convert.ToString(connectedPlayers + 1) + " has connected!"); 
 					clientList[connectedPlayers] = client;
 					connectedPlayers++;
+				}
+				Console.WriteLine("<< 2 clinets have connected to the the Pong2D server");
+				Console.WriteLine("<< Waiting for clients to send the start command....");
+
+				byte[] data1 = new byte[1024];
+				byte[] data2 = new byte[1024];
+
+				String mes1, mes2;
+				while(true)
+				{
+					NetworkStream stream1 = clientList[0].GetStream();
+					NetworkStream stream2 = clientList[1].GetStream();
+
+					for(int i = stream1.Read(data1,0,data1.Length);i != 0; i = stream1.Read(data1,0,data1.Length))
+					{
+						mes1 = System.Text.Encoding.ASCII.GetString(data1,0,i);
+						Console.WriteLine("Recieved: {0}", mes1);
+					}
+					for(int i = stream2.Read(data2,0,data2.Length);i != 0; i = stream2.Read (data2,0,data2.Length))
+					{
+						mes2 = System.Text.Encoding.ASCII.GetString(data2,0,i);
+						Console.WriteLine("Recieved: {0}", mes2);
+					}
+					Console.WriteLine("");
 				}
 			}
 			catch(Exception ex)
