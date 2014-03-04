6d70823c003079a119712e142a9e654c5f6e5fdc
putty174 <myh10232223@yahoo.com>
2014-03-03 17:25:55 -0800

    Revert "Server Receive Messages"
    This reverts commit 2d4e84a9a6b0ce24217b5558627fe36e1bef35b6.

--- MasterServer/MasterServer/Main.cs
+++ MasterServer/MasterServer/Main.cs
@@ -19,10 +19,7 @@ namespace MasterServer
 			Console.WriteLine(" >> " + "Wainting for clients.....");
 
 			MainServer ms = new MainServer();
-
-			while (true) {
-				ms.process();
-			}
+			
 		}
 
 
@@ -52,26 +49,6 @@ namespace MasterServer
 			listenThread2.Start ();
 			
 		}
-
-		public void process() {
-			byte[] data1 = new byte[1024];
-			byte[] data2 = new byte[1024];
-			
-			String mes1, mes2;
-
-			NetworkStream stream1 = clientList [0].GetStream ();
-			NetworkStream stream2 = clientList [1].GetStream ();
-			
-			for (int i = stream1.Read(data1,0,data1.Length); i != 0; i = stream1.Read(data1,0,data1.Length)) {
-				mes1 = System.Text.Encoding.ASCII.GetString (data1, 0, i);
-				Console.WriteLine ("Recieved: {0}", mes1);
-			}
-			for (int i = stream2.Read(data2,0,data2.Length); i != 0; i = stream2.Read (data2,0,data2.Length)) {
-				mes2 = System.Text.Encoding.ASCII.GetString (data2, 0, i);
-				Console.WriteLine ("Recieved: {0}", mes2);
-			}
-			Console.WriteLine ("");
-		}
 		
 		public void ListendForTCPClients()
 		{
@@ -88,6 +65,28 @@ namespace MasterServer
 				}
 				Console.WriteLine("<< 2 clinets have connected to the the Pong2D server");
 				Console.WriteLine("<< Waiting for clients to send the start command....");
+
+				byte[] data1 = new byte[1024];
+				byte[] data2 = new byte[1024];
+
+				String mes1, mes2;
+				while(true)
+				{
+					NetworkStream stream1 = clientList[0].GetStream();
+					NetworkStream stream2 = clientList[1].GetStream();
+
+					for(int i = stream1.Read(data1,0,data1.Length);i != 0; i = stream1.Read(data1,0,data1.Length))
+					{
+						mes1 = System.Text.Encoding.ASCII.GetString(data1,0,i);
+						Console.WriteLine("Recieved: {0}", mes1);
+					}
+					for(int i = stream2.Read(data2,0,data2.Length);i != 0; i = stream2.Read (data2,0,data2.Length))
+					{
+						mes2 = System.Text.Encoding.ASCII.GetString(data2,0,i);
+						Console.WriteLine("Recieved: {0}", mes2);
+					}
+					Console.WriteLine("");
+				}
 			}
 			catch(Exception ex)
 			{
