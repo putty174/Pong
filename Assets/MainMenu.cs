a7342192bf7124e6688f57598309789882baf8fb
JayhawkForLife <jayhawkfan999@gmail.com>
2014-03-07 12:13:39 -0800

    commit .gitignore

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	GUISkin skin;
+	public Texture2D logoTexture;
+
+	void OnGUI () {
+		GUI.Label (new Rect (0,0,100,50), logoTexture);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
