67f413086050f9fe7a0de2efdcaf3151e6e7b832
JordanLingHua <jordan.hua@yahoo.com>
2014-03-10 14:52:03 -0700

    Revert "assigning client which paddle to control implemented"
    This reverts commit 25a293f670d87b8be5cb27caa5cdd0dd1619563e.

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -8,7 +8,7 @@ using System;
 using System.Threading;
 
 public class Client : MonoBehaviour {
-	
+
 	const string serverLocation = "128.195.11.124";
 	const int portNumber = 4000;
 	public TcpClient client;
@@ -16,57 +16,57 @@ public class Client : MonoBehaviour {
 	public Thread t = null;
 	IPEndPoint serverEndPoint; 
 	public bool isConnected = false;
-	public Queue receiverBuffer;//Is assigned in TSock
-	
+	public Queue receiverBuffer;
+
 	//Positions of assets
-	
+
 	//These objects' properties gets sent to the server.  
 	//The server in return sends a message to player 1 and player 2 using this
-	
+
 	public GameObject player1;//send player1's position in byte msg
 	public GameObject player2;//send player2's position in byte msg
 	public GameObject ball;//send ball's position in byte msg
-	
+
 	public GameObject gameManager;//_gameManager object.  Send score
-	
+
 	public GameObject goal1;//uses it's position that occurs from goalscript.  
 	public GameObject goal2;//
-	
+
 	public GameObject GUI;//The GUIScript's buttons are used.  <---Don't know if need.  
 	//Others need for sure as of now.  
-	
-	
+
+
 	//1st person to connect is assigned player 1
 	//2nd person to connect is assigned player 2
-	
+
 	public static int playerThatClientControls;//if msg is 1, then assigned to player 1.  
 	//if msg is 2, then assigned to player 2.  Used in Player1Script.cs and Player2Script.cs
-	
-	
+
+
 	public Client()
 	{
 		serverEndPoint = new IPEndPoint(IPAddress.Parse(serverLocation), portNumber); 
 		receiverBuffer = new Queue ();
 	}
-	
+
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		//________SEND POSITIONS TO SERVER HERE_________
-		
-		
-		
-		
-		
-		
-		
-	}
 	
+		//________SEND POSITIONS TO SERVER HERE_________
+
+
+
+
+
+
+
+	}
+
 	public bool Connect()
 	{
 		try
@@ -86,25 +86,17 @@ public class Client : MonoBehaviour {
 			
 		}
 		if ( client == null ) return false;
-		
-		
+
+
 		//if(server.numOfClientsConnected = 2) playerThatClientControls = 2; 
 		//else playerThatClientControls = 1;
-		//if(nws == 1)
-		if(receiverBuffer.GetHashCode() == 1)
-		{
-			playerThatClientControls = 2;
-		}
-		else
-		{
-			playerThatClientControls = 1;
-		}
-		
-		
-		
+
+
+
+
 		return client.Connected;
 	}
-	
+
 	public bool Disconnect()
 	{
 		try
@@ -114,7 +106,7 @@ public class Client : MonoBehaviour {
 			if(!client.Connected)
 			{
 				t.Abort();
-				
+
 			}
 		}
 		catch(Exception ex)
@@ -123,7 +115,7 @@ public class Client : MonoBehaviour {
 		}
 		return true;
 	}
-	
+
 	public bool StartGame()
 	{
 		try
@@ -135,11 +127,11 @@ public class Client : MonoBehaviour {
 		{
 			print ( ex.Message + " : OnStartGame");
 		}
-		
-		
+
+
 		return true;
 	}
-	
+
 	public void Send(byte message)
 	{
 		try
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,78 +4,58 @@ using System.Collections;
 
 
 public class PlayerScript : MonoBehaviour {
-	
-	
+
+
 	public KeyCode up;
 	public KeyCode down;
 	public float paddleSpeed = .1f;
 	public float paddlePosition = 12.4f/32f;
 	public float acceleration = 30f;
-	
+
 	private float input;
-	
+
 	private bool isStopped = false;
-	
-	public GameObject playerToControl;
-	
-	
+
+
+
 	// Use this for initialization
 	void Start () {
-		
-		if(Client.playerThatClientControls == 1)
-		{
-			playerToControl = GameObject.Find("Player1");
-		}
-		else //if playerThatClientControls == 2
-		{
-			playerToControl = GameObject.Find ("Player2");
-		}
-		
+
+
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		//Vector2 temp = rigidbody2D.velocity;
-		
+
+		Vector2 temp = rigidbody2D.velocity;
+
 		// get the current position
-		//Vector2 pos = transform.position;
-		
-		
-		Vector2 temp = playerToControl.rigidbody2D.velocity;
-		
-		// get the current position
-		Vector2 pos = playerToControl.transform.position;
-		
-		
-		
+		Vector2 pos = transform.position;
 		
 		if (Input.GetKey(up)) 
 		{
 			// player wants to move the racket upwards
-			//rigidbody2D.velocity = new Vector2(0, paddleSpeed);
-			playerToControl.rigidbody2D.velocity = new Vector2(0, paddleSpeed);
+			rigidbody2D.velocity = new Vector2(0, paddleSpeed);
 		} 
 		else if (Input.GetKey(down)) 
 		{                       
 			// player wants to move the racket downwards
-			//rigidbody2D.velocity = new Vector2(0, paddleSpeed *-1);
-			playerToControl.rigidbody2D.velocity = new Vector2(0, paddleSpeed *-1);
+			rigidbody2D.velocity = new Vector2(0, paddleSpeed *-1);
 		}
 		else
 		{
-			//rigidbody2D.velocity = new Vector2(0,0);
-			playerToControl.rigidbody2D.velocity = new Vector2(0,0);
+			rigidbody2D.velocity = new Vector2(0,0);
 		}
 		
-		
+
 		
 		
 		
 		
 		
 	}
-	
+
 	public void OnTriggerEnter2D (Collider2D col) 
 	{
 		if (col.tag == "Wall") 
