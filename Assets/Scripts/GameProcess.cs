bfc20e7610d95275c49ed9e4579c39016b417b7d
Khai Nguyen <khaibn@uci.edu>
2014-03-11 18:58:12 -0700

    Added timeOfcollide method to GameProcess.cs

--- Assets/Scripts/GameProcess.cs
+++ Assets/Scripts/GameProcess.cs
@@ -1,9 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-using System;
-using System.Collections.Generic;
-
 //An example main function
 public class GameProcess : MonoBehaviour {
 
@@ -19,8 +16,17 @@ public class GameProcess : MonoBehaviour {
 	private Sockets sockets;
 	private Client client;
 	private GUIScript gui;
+    private Vector2 collisionLoc; //collision location
 
 	private int buffer;
+    private int opPosY;
+    private int opVel;
+    private int ballPosX;
+    private int ballPosY;
+    private int angle;
+    private int time;
+    private Vector2 hit;
+    private Vector2 pointOfCollision;
 
 	// Use this for initialization
 	void Start () {
@@ -34,6 +40,7 @@ public class GameProcess : MonoBehaviour {
 		p2 = GameObject.Find ("Player2");
 		ball = GameObject.Find ("GameBall");
 		bscript = (BallScript) ball.GetComponent("BallScript");
+
 	}
 	
 	// Update is called once per frame
@@ -75,29 +82,47 @@ public class GameProcess : MonoBehaviour {
 			{
 				while(client.receiverBuffer.Count > 0)
 				{
-					buffer = (int) client.receiverBuffer.Dequeue();
-					switch(buffer)
-					{
-					case 0:
-						if(player == -1)
-						{
-							player = 1;
-							Debug.Log("Player 1");
-						}
-						else
-						{
-							bscript.BallStart();
-							Debug.Log("Start");
-						}
-						break;
-					case 1:
-						if(player == -1)
-						{
-							player = 2;
-							Debug.Log("Player 2");
-						}
-						break;
-					}
+                    if (player == -1)
+                    {
+                        buffer = (int)client.receiverBuffer.Dequeue();
+                        switch (buffer)
+                        {
+                            case 0:
+                                if (player == -1)
+                                {
+                                    player = 1;
+                                    Debug.Log("Player 1");
+                                }
+                                else
+                                {
+                                    bscript.BallStart();
+                                    Debug.Log("Start");
+                                }
+                                break;
+                            case 1:
+                                if (player == -1)
+                                {
+                                    player = 2;
+                                    Debug.Log("Player 2");
+                                }
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        //Stores information on opponent position (Y), 
+                        //opponent velocity, ball position (X, Y),
+                        //angle of ball, server time.
+                        opPosY = (int)client.receiverBuffer.Dequeue();
+                        opVel = (int)client.receiverBuffer.Dequeue();
+                        ballPosX = (int)client.receiverBuffer.Dequeue();
+                        ballPosY = (int)client.receiverBuffer.Dequeue();
+                        angle = (int)client.receiverBuffer.Dequeue();
+                        time = (int)client.receiverBuffer.Dequeue();
+
+                    }
+                    
+                    
 				}
 			}
 		}
@@ -115,55 +140,29 @@ public class GameProcess : MonoBehaviour {
 		}
 	}
 
+    //return estimated time of collision.
+    //Parameters (ball position, ball angle, ball velocity)
+    public int timeOfcollide(Vector2 pos, int angle, int velocity)
+    {
+        float dirX = (float)(velocity * Mathf.Cos(angle));
+        float dirY = (float)(velocity * Mathf.Sin(angle));
+        Vector2 dir = new Vector2(dirX, dirY);
 
-	public void sendPositions ()
-	{
-		//********* COMPLETE THE FOLLOWING CODE
-		
-		try
-		{
-			if(player == 1)
-			{
-				//send Player1.x
-				//send Player1.y
-				//client.Send ((byte)Player1.player1PosX);
-				//Debug.Log ("Paddle 1 x position sent"+(byte)Player1.player1PosX);
+        ball.transform.position = pos;
+        ball.rigidbody2D.AddForce(dir);
 
-				client.Send ((byte)((int)(Player1.player1PosY * (250/13))));//player position * (manual byte range / boardwidth)
-				//Debug.Log ("Paddle 1 y position sent"+(byte)Player1.player1PosY);
-				Debug.Log ("Paddle 1 y position sent"+(byte)((int)(Player1.player1PosY * (250/13))));
+        RaycastHit2D raycastHit = Physics2D.Raycast(pos, dir);
+        pointOfCollision = raycastHit.point;
 
-				//number 0 to 250 is the number that server recognizes as a position.  
-				//number 251 is recognized as pause in the server
-				//number 252 is ..
-				//etc.  
+        float distance = Vector2.Distance(pos, pointOfCollision) - 0.1f;
+        
+        return (int)(distance / velocity);
+    }
 
-				
-			}
-			else if (player == 2)
-			{
-				//send Player2.x
-				//send Player2.y
-				//client.Send ((byte)Player2.player2PosX);
-				//Debug.Log ("Paddle 2 x position sent"+(byte)Player2.player2PosX);
-
-				client.Send ((byte)((int)(Player2.player2PosY * (250/13))));//player position * (manual byte range / boardwidth)
-				//Debug.Log ("Paddle 2 y position sent"+(byte)Player2.player2PosY);
-				Debug.Log ("Paddle 1 y position sent"+(byte)((int)(Player2.player2PosY * (250/13))));
-				
-				
-			}
-			
-		}
-		catch(Exception ex)
-		{
-			print ( ex.Message + " : Sending positions" );
-		}
-
-	}
-
-
-
+    public bool collide()
+    {
+        return true;
+    }
 
 	public Sockets returnSocket ()
 	{
